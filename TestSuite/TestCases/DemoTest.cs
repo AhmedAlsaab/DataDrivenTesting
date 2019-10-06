d41329050b591796f7a36b1e856659e8dfe29da0
Ahmed Alsaab <ahmed_alsaab@hotmail.com>
2019-10-07 00:20:49 +0100

    Initial DemoTest class created & WebDriver (Chrome) setup and fully running

---
+++ TestSuite/TestCases/DemoTest.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestSuite
+{
+    [TestFixture]
+    public class DemoTest 
+    {
+        public string chromeDriverPath = @"C:\Automation";
+        public static string testURL = "https://demoqa.com/html-contact-form/";
+        public static IWebDriver chrome;
+
+
+        // SetUp will run before the tests are called
+        [SetUp]
+        public void SetupAndPrepareChromeDriver()
+        {
+            chrome = new ChromeDriver(chromeDriverPath)
+            {
+                Url = testURL
+            };
+        }
+
+        [Test, Order(1)]
+        public void FirstContactFormTest()
+        {
+
+        }
+
+        [Test, Order(2)]
+        public void SecondContactFormTest()
+        {
+
+        }
+
+        [Test, Order(3)]
+        public void ThirdContactFormTest()
+        {
+
+        }
+
+        // TearDown will make sure all garbage (chrome, processes etc) are closed down
+        [TearDown]
+        public void TearDown()
+        {
+            chrome.Close();
+            chrome.Quit();
+        }
+    }
+}
