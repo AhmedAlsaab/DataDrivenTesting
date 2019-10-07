dc229aad78f29c849b55106d41e8fedf4eee4fea
Ahmed Alsaab <ahmed_alsaab@hotmail.com>
2019-10-07 16:38:42 +0100

    Created a different variation of the ChromeService class (ThreadedChromeService) which can be used to initialize a new thread for each chrome instance. This class is for demo purposes, ideally, all WebDriver logic should be encapsulated within one type of 'service' class

---
+++ TestSuite/Service/ThreadedChromeService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SE = SeleniumExtras;
+using System.Threading;
+using NUnit.Framework;
+
+namespace TestSuite.Service
+{
+    class ThreadedChromeService
+    {
+        readonly int secondsToWait = 5;
+
+        // Starting a new thread for each driver (chrome) instance
+        public static ThreadLocal<IWebDriver> driverInstance = new ThreadLocal<IWebDriver>();
+
+        // Invoked for WebDriver operations
+        public static IWebDriver GetDriver()
+        {
+
+            return driverInstance.Value;
+        }
+
+        // Setting the argument (driver) to the thread created
+        public static void SetDriver(IWebDriver driver)
+        {
+
+            driverInstance.Value = driver;
+
+        }
+
+        // Tracking how many threads are created via ID
+        public static void TrackThreads()
+        {
+            var listThreadIds = Thread.CurrentThread.ManagedThreadId.ToString();
+            System.Diagnostics.Debug.Write("Thread Id: \n", listThreadIds);
+        }
+
+        public static void CleanThreadsUp()
+        {
+            driverInstance.Dispose();
+        }
+
+    }
+}
