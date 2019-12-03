333297cb8e303c8ab568dcf8bdcafe511f2af0ad
Ahmed <46319030+AhmedAlsaab@users.noreply.github.com>
2019-12-03 13:44:17 +0000

    Updated ExcelFileReader with new logic

--- ExcelOperations/ExcelFileReader.cs
+++ ExcelOperations/ExcelFileReader.cs
@@ -1,24 +1,27 @@
-﻿using System;
-using excel = Microsoft.Office.Interop.Excel;
-
-namespace ExcelOperations
+namespace AutomationExcel
 {
     public class ExcelFileReader
     {
+        readonly String ExcelFileLocationHome = @"Your Location";
+        readonly String ExcelFileLocationWork = @"Your Location";
 
-        readonly string ExcelFileLocationHome = @"C:\Users\a\Documents\DDT\DataDrivenTesting\ExcelOperations\Data\TestData.xlsx";
-
-
-        // Look values up from Excel through row num, colum num and specify which sheet to look in
+        // Lookup values from Excel Workbook to use as data in Automation
         public string ExcelLookup(int x, int y, int sheetNum)
         {
             excel.Application Xapp = new excel.Application();
-            excel.Workbook xWorkbook = Xapp.Workbooks.Open(ExcelFileLocationHome);
+            excel.Workbook xWorkbook = Xapp.Workbooks.Open(ExcelFileLocationWork);
             try
             {
                 excel.Worksheet xWorksheet = xWorkbook.Sheets[sheetNum];
                 excel.Range xRange = xWorksheet.UsedRange;
                 return xRange.Cells[x][y].Value.ToString();
+
+            } catch (RuntimeBinderException)
+            {
+                string emptyValue = "Empty/Invalid Value: Check your selected data set!";
+                System.Diagnostics.Debug.WriteLine("Null value found, check Excel values");
+                Console.Write("Null value found, check Excel values");
+                return emptyValue;
             }
             // Release Excel & Quit (Prevents Excel processes from ramping up)
             finally
@@ -28,6 +31,24 @@ namespace ExcelOperations
                 System.Runtime.InteropServices.Marshal.ReleaseComObject(Xapp);
             }
         }
+        // Writing Quote Results to Excel
+        public void SaveToExcel(int x, int y, string dataToSend, int sheetNum)
+        {
+            excel.Application Xapp = new excel.Application();
+            excel.Workbook xWorkbook = Xapp.Workbooks.Open(ExcelFileLocationWork);
+            try
+            {
+                excel.Worksheet xWorksheet = xWorkbook.Sheets[sheetNum];
+                excel.Range xRange = xWorksheet.UsedRange;
+                xRange.Cells[x, y] = dataToSend;
+                xWorkbook.Save();
+            }
+            finally
+            {
+                xWorkbook.Close(true);
+                Xapp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(Xapp);
+            }
+        }
     }
-       
 }
